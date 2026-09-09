3ecdd345ea061827c7bc8801133718c433b437f8
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2026-09-09 13:57:19 -0600

    Try RelWithDebInfo

--- mache/jigsaw/build.sh
+++ mache/jigsaw/build.sh
@@ -8,9 +8,9 @@ mkdir build
 cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   ../external/jigsaw
-cmake --build . --config Release --target install
+cmake --build . --config RelWithDebInfo --target install
 cd ..
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
